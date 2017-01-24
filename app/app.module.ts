b29c9ff0064ee911226f395fbfac9ebcd56d2830
System Administrator <root@Anis-Darwich-MacBook-Pro.local>
2017-01-24 23:12:04 +0100

    chapitre 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './app.hero-detail.component';
 
 @NgModule({
   imports:      [
